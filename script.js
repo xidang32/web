2826b5ae2d3f9359cb46822e66daa6fec72d1824
daitoan.dang <daitoan.dang@ucalgary.ca>
2025-08-23 15:14:31 +0700

    beta

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.querySelector(".add").addEventListener("click", () => {
+    document.body.classList.remove("show-bg");
+    document.body.classList.add("hide-bg");
+});
+
+document.querySelector(".buy").addEventListener("click", () => {
+    document.body.classList.remove("hide-bg");
+    document.body.classList.add("show-bg");
+});
